15025aa1e0da3e1df2de05ec26cf27a67ea08177
Huckleberry <huckleberry@wrtn.io>
2024-04-07 17:18:49 +0900

    chore: test nested object

--- src/editor/editor.ts
+++ src/editor/editor.ts
@@ -1,9 +1,10 @@
 import { makeObservable, observable, computed, action, flow } from 'mobx';
 
-async function randomData() {
+async function randomData(): Promise<IEditorData> {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   return {
     title: 'Hello World',
+    depth: {},
   };
 }
 
@@ -11,33 +12,55 @@ interface IRandomDataResponse {
   title: string;
 }
 
+interface IEditorData {
+  title: string;
+  depth: Record<string, any>;
+}
+
 class Editor {
   public readonly _id: string;
+
+  public generating = false;
+
+  public data: IEditorData = {
+    title: '',
+    depth: {},
+  };
 
   constructor() {
     this._id = Date.now().toString();
 
     makeObservable(this, {
-      data: observable,
+      data: observable.shallow,
       updateData: action,
+      generate: flow,
+      generating: observable,
     });
   }
 
-  public updateData(key: string, value: any) {
-    this.data[key] = value;
+  public updateData<T extends keyof IEditorData>(
+    key: T,
+    value: T extends 'depth' ? Record<string, any> : IEditorData[T],
+  ) {
+    if (key === 'depth') {
+      // @ts-ignore
+      this.data.depth = { ...this.data.depth, ...value };
+    } else {
+      // @ts-ignore
+      this.data[key] = value;
+    }
   }
 
-  public data: Record<string, any> = {
-    title: '',
-  };
-
-  public *generate(): Generator<
-    Promise<IRandomDataResponse>,
-    void,
-    IRandomDataResponse
-  > {
-    const response = yield randomData();
-    this.data = response;
+  public *generate(): Generator<Promise<IEditorData>, void, IEditorData> {
+    try {
+      this.generating = true;
+      const response = yield randomData();
+      this.data = response;
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.generating = false;
+    }
   }
 }
 
--- src/editor/editor.ui.tsx
+++ src/editor/editor.ui.tsx
@@ -1,7 +1,7 @@
 import { observer } from 'mobx-react-lite';
 import React, { memo } from 'react';
 import Editor from './editor';
-import { StyleSheet, TextInput, View } from 'react-native';
+import { ActivityIndicator, Button, TextInput, View } from 'react-native';
 
 const EditorView = observer<{ editor: Editor }>(({ editor }) => {
   console.log('rerender', editor._id);
@@ -14,13 +14,31 @@ const EditorView = observer<{ editor: Editor }>(({ editor }) => {
         borderRadius: 10,
         borderColor: '#eeeeee99',
         borderWidth: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
       }}>
       <TextInput
+        multiline
         value={editor.data.title}
         onChangeText={(value) => {
           editor.updateData('title', value);
         }}
-        style={StyleSheet.absoluteFill}
+        style={[{ flex: 1, backgroundColor: 'pink' }]}
+      />
+      <TextInput
+        multiline
+        value={editor.data.depth.body ?? ''}
+        onChangeText={(value) => {
+          editor.updateData('depth', { body: value });
+        }}
+        style={[{ flex: 1, backgroundColor: 'blue' }]}
+      />
+      {editor.generating ? <ActivityIndicator size="small" /> : null}
+      <Button
+        title="generate"
+        onPress={() => {
+          editor.generate();
+        }}
       />
     </View>
   );
